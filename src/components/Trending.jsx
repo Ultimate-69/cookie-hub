0e8eecb3e0c43f6addcf599c942faec875eb0e58
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-03-04 20:47:25 +0200

    implemented the base for the movies

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,14 +1,50 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Oval } from 'react-loader-spinner'
+import MovieCard from './MovieCard';
 
 export default function Trending() {
 
     const [isLoaded, setisLoaded] = useState(false);
+    const [movies, setMovies] = useState([]);
+    const apiKey = import.meta.env.VITE_READ_API_KEY;
+
+    function fetchMovies() {
+        const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${apiKey}`
+            }
+        };
+
+        const movieData = fetch(url, options);
+        movieData.then((response) => {
+            response.json().then((data) => {
+                setMovies(data.results);
+                setisLoaded(true);
+            })
+        })
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, [])
 
   return (
     <div className='flex justify-center items-center flex-col'>
         <h1 className='mb-5 text-4xl font-bold'>Trending Movies</h1>
-        {isLoaded ? <div></div> : <Oval height={50} width={50} />}
+        {isLoaded ?
+         <div className='flex flex-wrap justify-center items-center'>
+            {movies.map((movie) => (
+                <div key={movie.id}>
+                    <MovieCard movie={movie} />
+                </div>
+            ))}
+         </div> 
+         : 
+         <Oval height={50} width={50} />
+        }
     </div>
   )
 }
